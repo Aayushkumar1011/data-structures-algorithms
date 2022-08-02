0fffcd4404aa5706d9d3184bf15d3365e7fe76c5
Ayush kumar bharti <aayushkumar1011@gmail.com>
2022-08-02 18:21:06 +0530

    Update LongestIncreasingSubsequence.java

--- dynamic-programming/LongestIncreasingSubsequence.java
+++ dynamic-programming/LongestIncreasingSubsequence.java
@@ -1,5 +1,5 @@
 //300. Longest Increasing Subsequence
-class LongestIncreasingSubsequence {
+ class LongestIncreasingSubsequence {
     public int lengthOfLIS(int[] nums) {
         if(nums.length <= 1)
             return nums.length;
@@ -8,14 +8,12 @@ class LongestIncreasingSubsequence {
         int res = 1;
         Arrays.fill(dp, 1);
         for(int i = 1; i < size; i++){
-            int curLIS = 0;
             for(int j = 0; j < i; j++){
-                if(nums[j] < nums[i]){
-                    curLIS = Math.max(curLIS, dp[j]);
+                if(nums[j] < nums[i] && dp[i] < 1 + dp[j]){
+                    dp[i] = 1 + dp[j];
                 }
+                res = Math.max(res, dp[i]);
             }
-            dp[i] = 1 + curLIS;
-            res = Math.max(res, dp[i]);
         }
         return res;
     }
